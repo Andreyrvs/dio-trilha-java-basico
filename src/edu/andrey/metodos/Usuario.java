8df9a730acb1e7fc8d75b49f9fc9eb1f484f7734
Andrey R. Visniewski <andreyrvdev@gmail.com>
2025-03-09 22:59:46 -0300

    capitulo 5

---
+++ src/edu/andrey/metodos/Usuario.java
@@ -0,0 +1,4 @@
+package edu.andrey.metodos;
+
+public class Usuario {
+}
---
+++ src/edu/andrey/operadores/Operadores.java
@@ -0,0 +1,21 @@
+package edu.andrey.operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        String resultado = "";
+        if (true) {
+            resultado = "verdaderio";
+        } else {
+            resultado = "falso";
+        }
+
+        System.out.println(resultado);
+        boolean simNao = a == b;
+        System.out.println(simNao);
+    }
+
+
+}
