1c31fae9534f19878685eec5effc1cc91fce3c5c
Andrey R. Visniewski <andreyrvdev@gmail.com>
2025-03-09 23:12:18 -0300

    capitulo 6

--- src/edu/andrey/metodos/Usuario.java
+++ src/edu/andrey/metodos/Usuario.java
@@ -1,4 +1,24 @@
 package edu.andrey.metodos;
 
 public class Usuario {
+    public static void  main(String[] args) {
+        SmartTv smartTv = new SmartTv();
+        System.out.println("Tv ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        smartTv.mudarDeCanal(15);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+        smartTv.ligar();
+        System.out.println("novo estado:" + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("novo estado:" + smartTv.ligada);
+
+        smartTv.aumentarVolume();
+        System.out.println("Aumentando volume: " + smartTv.volume);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        System.out.println("Diminuir volume: " + smartTv.volume);
+    }
 }
